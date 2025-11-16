427aac57158830c70ee8a68d74018197bb6db001
Gabe Anguiano <gabe.anguiano@gmail.com>
2025-11-16 13:01:51 -0600

    adding new exclusion

--- Maintain-FolderHashes.ps1
+++ Maintain-FolderHashes.ps1
@@ -1,5 +1,5 @@
 . ./File-Hashing-Utilities.ps1
 
-MaintainFolderHashes -BaseFolderPaths @("/mnt/storage/") -ExclusionCriteria @(".*\.plex.*", ".*\.git.*") 
+MaintainFolderHashes -BaseFolderPaths @("/mnt/storage/") -ExclusionCriteria @(".*\.plex.*", ".*\.git.*", "/mnt/storage/Configuration/Docker/.*") 
 
 Write-Host "Done!"
